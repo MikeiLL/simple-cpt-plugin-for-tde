d2566c38e31967db1d91b624af265e06f178fee1
mikeill <mike@mzoo.org>
2026-05-20 13:07:57 -0500

    Add acf fields.

--- tdeplugin.php
+++ tdeplugin.php
@@ -118,6 +118,70 @@ function cptui_register_tde_cpts() {
 
 add_action( 'init', 'cptui_register_tde_cpts' );
 
+add_action( 'acf/include_fields', function() {
+	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+		return;
+	}
+
+	acf_add_local_field_group( array(
+	'key' => 'group_6a0df490c10c3',
+	'title' => 'Personnel',
+	'fields' => array(
+		array(
+			'key' => 'field_6a0df4912cd1e',
+			'label' => 'Roles',
+			'name' => 'roles',
+			'aria-label' => '',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'maxlength' => '',
+			'allow_in_bindings' => 0,
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'board-member',
+			),
+		),
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'artist',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+	'display_title' => '',
+	'allow_ai_access' => false,
+	'ai_description' => '',
+) );
+} );
+
+
+
 add_shortcode('board', function($atts) {
   $posts = get_posts([
         'post_type' => 'board-member',
